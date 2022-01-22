2556c76a4da5f036d141599c51e04f133daea536
natimati <95073484+natimati@users.noreply.github.com>
2022-01-22 18:34:23 +0100

    adding styled-components with theme, creating example styled component (#1)
    * adding styled-components with theme, creating example styled component
    
    * moving theme to seperate file
    
    * removing index.css and adding global styles from styles components
    
    * skipping unused(for now) test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "styled-components";
+import { theme, GlobalStyle } from "./styles";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/styles.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+`;
+
+export const theme = {
+  colors: {
+    ui: "#BF7D65",
+    white: "#fff",
+  },
+};
