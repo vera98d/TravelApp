40009065056c5e5f8901d40d134db9c67e15a5ae
JaCoWymowny <80853701+JaCoWymowny@users.noreply.github.com>
2022-01-30 19:33:39 +0100

    Add HistoryEntry component to project (#8)

---
+++ src/components/HistoryEntry/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyledHistoryEntry, StyledStoryText } from "./style";
+
+const HistoryEntry = (props) => {
+  return (
+    <StyledHistoryEntry>
+      <h1 className="storyContentTitle">
+        {props.title}-{props.city}
+      </h1>
+      <div className="storyContent">
+        <img src={props.image} className="currentStoryImage" />
+        <StyledStoryText>{props.description}</StyledStoryText>
+      </div>
+    </StyledHistoryEntry>
+  );
+};
+
+export default HistoryEntry;
---
+++ src/components/HistoryEntry/style.js
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+
+export const StyledHistoryEntry = styled.div`
+  display: flex;
+  width: 100%;
+  height: 80%;
+  position: relative;
+  flex-wrap: wrap;
+
+  .storyContentTitle {
+    width: 100%;
+    height: 80px;
+    text-align: center;
+  }
+
+  .currentStoryImage {
+    width: 15em;
+    height: 25em;
+    border-radius: 30px;
+    margin-right: 3%;
+  }
+
+  .storyContent {
+    display: flex;
+    height: 25em;
+    text-align: left;
+  }
+
+  @media screen and (min-width: 768px) {
+    .currentStoryImage {
+      width: 18em;
+    }
+  }
+`;
+
+export const StyledStoryText = styled.div`
+  width: 60%;
+  margin: 0 1em;
+  overflow: auto;
+  font-size: 12px;
+
+  &::-webkit-scrollbar {
+    width: 10px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: ${(props) => props.theme.colors.primaryUi};
+    border-radius: 5px;
+  }
+  &::-webkit-scrollbar-track {
+    background: ${(props) => props.theme.colors.white};
+    border-radius: 5px;
+  }
+
+  > * {
+    margin-bottom: 12px;
+  }
+
+  @media screen and (min-width: 768px) {
+    font-size: 16px;
+  }
+`;
